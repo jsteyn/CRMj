ee2643376a256deb89f84165a997e052692d23ff
Stuart Lewis <stuart.manfred.lewis@gmail.com>
2023-06-16 15:18:11 +0100

    Add basic template

--- src/main/java/crmj/CRMjServer.java
+++ src/main/java/crmj/CRMjServer.java
@@ -1,10 +1,16 @@
 package crmj;
 
+import org.apache.commons.collections.map.HashedMap;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import spark.ModelAndView;
 import spark.Spark;
+import spark.template.velocity.VelocityTemplateEngine;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
 
 public class CRMjServer {
     private static final Logger s_LOGGER = LoggerFactory.getLogger(CRMjServer.class);
@@ -34,6 +40,11 @@ public class CRMjServer {
     }
 
     private void configureWebpages() {
-        Spark.get("/", (request, response) -> "Hello World");
+        VelocityTemplateEngine engine = new VelocityTemplateEngine();
+        Spark.get(
+            "/",
+            (request, response) -> new ModelAndView(new HashMap<String, Object>(), "website/index.vm"),
+            engine
+        );
     }
 }
