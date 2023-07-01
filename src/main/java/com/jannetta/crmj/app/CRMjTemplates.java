9a8d2d2a3e913217fef667c05b8bf8a022fb4f23
Stuart Lewis <stuart.manfred.lewis@gmail.com>
2023-07-01 13:17:27 +0100

    Implement template inheritance

--- src/main/java/com/jannetta/crmj/app/CRMjTemplates.java
+++ src/main/java/com/jannetta/crmj/app/CRMjTemplates.java
@@ -8,13 +8,20 @@ import spark.template.velocity.VelocityTemplateEngine;
 import java.util.HashMap;
 
 public class CRMjTemplates {
-    private static final TemplateViewRoute index = (request, response) -> basicRouteFromTemplate("velocity/index.vm");
+    private static final TemplateViewRoute index = (request, response) -> defaultTemplate("CRMj", "velocity/index.vm");
 
     public static void mapRoutes(VelocityTemplateEngine engine) {
         Spark.get("/", CRMjTemplates.index, engine);
     }
 
-    private static ModelAndView basicRouteFromTemplate(String templatePath) {
+    private static ModelAndView defaultTemplate(String title, String contentSource) {
+        HashMap<String, Object> data = new HashMap<>();
+        data.put("title", title);
+        data.put("content", contentSource);
+        return new ModelAndView(data, "velocity/layouts/default.vm");
+    }
+
+    private static ModelAndView basicTemplate(String templatePath) {
         return new ModelAndView(new HashMap<>(), templatePath);
     }
 }
