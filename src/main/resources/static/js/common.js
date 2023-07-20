41c41abec6c9ed1695e0e60173a6df3d87670564
Stuart Lewis <stuart.manfred.lewis@gmail.com>
2023-07-20 15:02:51 +0100

    Generify ajax error handling

--- src/main/resources/static/js/common.js
+++ src/main/resources/static/js/common.js
@@ -1,7 +1,13 @@
 function runAjax(type, url, data, onload) {
     console.log("Sending request [", url, "] with data [", data, "]");
     const xhttp = new XMLHttpRequest();
-    xhttp.onload = onload;
+    xhttp.onload = function() {
+        let response = JSON.parse(this.response);
+        if (response["success"])
+            onload(response);
+        else
+            alert(`Server error:\n${response["error"]}`);
+    };
     xhttp.open(type, url, true);
     xhttp.send(JSON.stringify(data));
 }
