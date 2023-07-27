c14041b58914ee9554ee600ae6db6bbed83cd7b8
Stuart Lewis <stuart.manfred.lewis@gmail.com>
2023-07-27 14:34:13 +0100

    Implement full crud operations on people

--- src/main/java/com/jannetta/crmj/app/CRMjServerAjaxManager.java
+++ src/main/java/com/jannetta/crmj/app/CRMjServerAjaxManager.java
@@ -29,6 +29,9 @@ public class CRMjServerAjaxManager {
     public void mapRoutes() {
         Spark.post("/get_person", wrapAjax(this::getPerson));
         Spark.post("/get_person_list_ranged", wrapAjax(this::getPersonListRanged));
+        Spark.post("/add_person", wrapAjax(this::addPerson));
+        Spark.post("/update_person", wrapAjax(this::updatePerson));
+        Spark.post("/remove_person", wrapAjax(this::removePerson));
     }
 
     private Route wrapAjax(Route route) {
@@ -46,7 +49,7 @@ public class CRMjServerAjaxManager {
         };
     }
 
-    private JsonObject getPerson(@NotNull Request request, @NotNull Response response) {
+    private @NotNull JsonObject getPerson(@NotNull Request request, @NotNull Response response) {
         JsonObject parameters = JsonParser.parseString(request.body()).getAsJsonObject();
         int personId = parameters.get("personId").getAsInt();
 
@@ -82,7 +85,7 @@ public class CRMjServerAjaxManager {
         JsonArray personList = new JsonArray();
         for (Object[] record : personListRaw) {
             JsonObject personData = new JsonObject();
-            personData.addProperty("id", (Integer) record[0]);
+            personData.addProperty("personId", (Integer) record[0]);
             personData.addProperty("firstName", (String) record[1]);
             personData.addProperty("lastName", (String) record[2]);
             personList.add(personData);
@@ -92,6 +95,50 @@ public class CRMjServerAjaxManager {
         output.add("people", personList);
 
         return output;
+    }
+
+    private JsonObject addPerson(@NotNull Request request, @NotNull Response response) {
+        Person person = new Gson().fromJson(request.body(), Person.class);
+
+        try (m_databaseManager) {
+            m_databaseManager.open();
+            m_databaseManager.create(person);
+        }
+
+        return new JsonObject();
+    }
+
+    private JsonObject updatePerson(@NotNull Request request, @NotNull Response response) {
+        Person person = new Gson().fromJson(request.body(), Person.class);
+
+        try (m_databaseManager) {
+            m_databaseManager.open();
+            HashMap<String, Object> queryParams = new HashMap<>();
+            queryParams.put("id", person.getId());
+            Person existing = m_databaseManager.readFrom(Person.class, "m_id = :id", queryParams).get(0);
+            existing.setFirstName(person.getFirstName());
+            existing.setMiddleNames(person.getMiddleNames());
+            existing.setLastName(person.getLastName());
+            existing.setTitle(person.getTitle());
+            m_databaseManager.update(existing);
+        }
+
+        return new JsonObject();
+    }
+
+    private JsonObject removePerson(@NotNull Request request, @NotNull Response response) {
+        JsonObject parameters = JsonParser.parseString(request.body()).getAsJsonObject();
+        int id = parameters.get("personId").getAsInt();
+
+        try (m_databaseManager) {
+            m_databaseManager.open();
+            HashMap<String, Object> queryParams = new HashMap<>();
+            queryParams.put("id", id);
+            Person person = m_databaseManager.readFrom(Person.class, "m_id = :id", queryParams).get(0);
+            m_databaseManager.delete(person);
+        }
+
+        return new JsonObject();
     }
 
     private String generateErrorReturn(String message) {
