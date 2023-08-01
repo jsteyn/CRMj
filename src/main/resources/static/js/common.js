ce232b99b64e7bf3e20443188e48f3f59ead5d74
Stuart Lewis <stuart.manfred.lewis@gmail.com>
2023-08-01 12:22:29 +0100

    Document runAjax function

--- src/main/resources/static/js/common.js
+++ src/main/resources/static/js/common.js
@@ -1,13 +1,27 @@
-function runAjax(type, url, data, onload) {
+/**
+ * Send an ajax request with the provided parameters.
+ * @param {string} type Request type (post, get, etc.).
+ * @param {string} url Route to the desired ajax call.
+ * @param {?Object} data Data to be passed to the server.
+ * @param {?function(Object):void} onload Callback to be executed on successful response.
+ * @param {?function(Object):void} onerror Callback to be executed on failed response (where a server error has been
+ * caught).
+ */
+function runAjax(type, url, data, onload, onerror) {
     console.log("Sending request [", url, "] with data [", data, "]");
     const xhttp = new XMLHttpRequest();
     xhttp.onload = function() {
         let response = JSON.parse(this.response);
         console.log("Received response [", response, "]");
-        if (response["success"])
-            onload(response);
-        else
-            alert(`Server error:\n${response["error"]}`);
+        if (response["success"]) {
+            if (onload)
+                onload(response);
+        } else {
+            if (onerror)
+                onerror(response);
+            else
+                alert(`Server error:\n${response["error"]}`);
+        }
     };
     xhttp.open(type, url, true);
     xhttp.send(JSON.stringify(data));
